d0a93ca213fb5e712efaf69e89cd98c8101097e9
Ayachicken <digimonmon.a@gmail.com>
2020-06-18 14:31:26 +0000

    [add]管理者側サーチコントローラー・ルーティング

--- config/routes.rb
+++ config/routes.rb
@@ -34,13 +34,14 @@ Rails.application.routes.draw do
       post 'admins/sign_in' => 'admins/sessions#create', as: 'admin_session'
       delete 'admins/sign_out' => 'admins/sessions#destroy',as: 'destroy_admin_session'
     end
-    
+
   namespace :admins do
     resources :posts, only:[:index, :show, :destroy]
     resources :scenes, only:[:index, :create, :destroy]
     resources :users, only:[:index]
     patch 'users/:id' => 'users#suspend',as: 'suspend_user'
-    get 'top' => 'homes#top',as: 'admin_top'
+    get 'top' => 'homes#top',as: 'top'
+    get 'search' => 'searches#search',as: 'search'
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
