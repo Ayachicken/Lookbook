8f6498a1362865cbb691b96a6340717ce36ef2b2
Ayachicken <digimonmon.a@gmail.com>
2020-07-02 15:45:21 +0000

    本番環境で検索ができなくなるエラー(検証中)

--- config/application.rb
+++ config/application.rb
@@ -22,9 +22,5 @@ module Lookbook
     config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}').to_s]
     config.i18n.default_locale = :ja
 
-    # rails cできないエラー対策
-    if Rails.env.deveropment?
-      config.web_console.whitelisted_ips = '0.0.0.0/0'
-    end
   end
 end
